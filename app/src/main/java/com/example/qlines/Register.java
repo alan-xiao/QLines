a0a4c1cfeb5d960896605d57b12009376e45cbd2
alanx99 <alanx99@gmail.com>
2020-02-08 00:58:45 -0500

    oof

---
+++ app/src/main/java/com/example/qlines/Register.java
@@ -0,0 +1,30 @@
+package com.example.qlines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText email, password;
+    Button registerBtn;
+    TextView loginBtn;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.registerBtn);
+        loginBtn = findViewById(R.id.createText);
+
+        fAuth = FirebaseAuth.getInstance();
+    }
+}
